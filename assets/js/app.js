30e278a54906f0d62cb85553c82bcebff2276673
Andrew Timberlake <andrew@andrewtimberlake.com>
2023-09-12 15:06:15 +0200

    Some changes made after writing the blog post

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,6 +24,7 @@ import topbar from "../vendor/topbar"
 // npm install @popperjs/core --prefix assets
 import { createPopper } from '@popperjs/core';
 
+// A class to manage the tooltip lifecycle.
 class Tooltip {
   showEvents = ['mouseenter', 'focus'];
   hideEvents = ['mouseleave', 'blur'];
@@ -46,6 +47,7 @@ class Tooltip {
     });
     this.destructors = [];
 
+    // For each show event, add an event listener on the parent element and store a destructor to call removeEventListener when the tooltip is destroyed.
     this.showEvents.forEach((event) => {
       const callback = this.show.bind(this);
       this.$parent.addEventListener(event, callback);
@@ -54,6 +56,7 @@ class Tooltip {
       );
     });
 
+    // For each hide event, add an event listener on the parent element and store a destructor to call removeEventListener when the tooltip is destroyed.
     this.hideEvents.forEach((event) => {
       const callback = this.hide.bind(this);
       this.$parent.addEventListener(event, callback);
@@ -63,22 +66,26 @@ class Tooltip {
     });
   }
 
-  destroy() {
-    this.destructors.forEach((destructor) => destructor());
-    this.popperInstance?.destroy();
-  }
-
+  // The show method adds the data-show attribute to the tooltip element, which makes it visible (see CSS).
   show() {
     this.$tooltip.setAttribute('data-show', '');
     this.update();
   }
 
+  // Update the popper instance so the tooltip position is recalculated.
+  update() {
+    this.popperInstance?.update();
+  }
+
+  // The hide method removes the data-show attribute from the tooltip element, which makes it invisible (see CSS).
   hide() {
     this.$tooltip.removeAttribute('data-show');
   }
 
-  update() {
-    this.popperInstance?.update();
+  // The destroy method removes all event listeners and destroys the popper instance.
+  destroy() {
+    this.destructors.forEach((destructor) => destructor());
+    this.popperInstance?.destroy();
   }
 }
 
